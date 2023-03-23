2dc473eb505693d2e7f4d73b050debdcc1c29b43
Lay <2892739899@qq.com>
2023-03-24 00:00:09 +0800

    first commit

---
+++ src/main/java/com_wjx_study_java/Git_test.java
@@ -0,0 +1,11 @@
+package com_wjx_study_java;
+
+/**
+ * @author wjx
+ * @version 1.0
+ */
+public class Git_test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
